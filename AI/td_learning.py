e2f9da0e6eb13d3e90d886969b97123c2983b389
Kai Jorgensen <jorgensen42@gmail.com>
2015-12-02 15:52:55 -0800

    td-learning working, 90+ percent wins with utils.p
    just need to do commenting/refactoring now

--- AI/td_learning.py
+++ AI/td_learning.py
@@ -1,8 +1,11 @@
-import math, heapq, sys, pickle
+import math, pickle
 from Player import *
 from Construction import Construction
 from Ant import Ant
 from AIPlayerUtils import *
+
+MAX_RAND_MOVES = 1000000
+GAMMA = .95
 
 ##
 # AIPlayer
@@ -32,24 +35,25 @@ class AIPlayer(Player):
         # reference to AIPlayer's anthill structure
         self.playerAnthill = None
 
-        # initialize our food location tracker to be empty
-        self.foodList = []
+        # states the agent has seen so far (keys) with their utilities (values)
+        self.stateUtils = {}
 
-        # neural network node values
-        self.nodes = []
+        # number of moves made so far by TD-Learning
+        self.moveCount = 0
 
-        # # initialize the neural network
-        # if LOAD_WEIGHTS_FROM_FILE:
-        #     # use weights previously saved to a file
-        #     f = open("jorgense12_asuncion16_hw8.p", 'rb')
-        #     weightsFromFile = pickle.load(f)
-        #     self.firstWeights = weightsFromFile[0]
-        #     self.secondWeights = weightsFromFile[1]
-        #     f.close()
-        # else:
-        #     print "Finding best weights... (takes a long time)"
-        #     # use random weights
-        #     self.initNetworkWeights()
+        # number of moves made this game
+        self.moveCountThisGame = 0
+
+        # load the state utilities from file if possible
+        try:
+            with open("utils.p", 'rb') as f:
+                data = pickle.load(f)
+                self.stateUtils = data[0]
+                self.moveCount = data[1]
+                print "Loading utils file..."
+                print `len(self.stateUtils)` + " entries loaded."
+        except IOError:
+            print "No utils file, starting from scratch..."
 
     ##
     # getSimpleState
@@ -67,17 +71,14 @@ class AIPlayer(Player):
         playerQueen = playerInv.getQueen()
         enemyQueen = enemyInv.getQueen()
 
-        # INT 0: defeat state (true/false)
-        if playerQueen is None or enemyInv.foodCount >= 11:
+        # INT 0: victory state (true/false)
+        if enemyQueen is None or playerInv.foodCount >= 11:
             int0 = 1
         else:
             int0 = 0
 
-        # INT 1: victory state (true/false)
-        if enemyQueen is None or playerInv.foodCount >= 11:
-            int1 = 1
-        else:
-            int1 = 0
+        # INT 1: player food count
+        int1 = playerInv.foodCount
 
         # INT 2: player queen is on anthill (true/false)
         if playerQueen and playerQueen.coords == self.playerAnthill:
@@ -98,23 +99,19 @@ class AIPlayer(Player):
         if enemyQueen and len(workerList) > 0:
             int5 = (abs(workerList[0].coords[0] - enemyQueen.coords[0]) +
                               abs(workerList[0].coords[1] - enemyQueen.coords[1]))
+            if len(workerList) > 1:
+                int5 += (abs(workerList[1].coords[0] - enemyQueen.coords[0]) +
+                                  abs(workerList[1].coords[1] - enemyQueen.coords[1]))
         else:
             int5 = 0
 
-        # INT 6: worker 2 distance to enemy queen
-        if enemyQueen and len(workerList) > 1:
-            int6 = (abs(workerList[1].coords[0] - enemyQueen.coords[0]) +
-                              abs(workerList[1].coords[1] - enemyQueen.coords[1]))
+        # INT 6: enemy queen health
+        if enemyQueen:
+            int6 = enemyQueen.health
         else:
             int6 = 0
 
-        # INT 7: enemy queen health
-        if enemyQueen:
-            int7 = enemyQueen.health
-        else:
-            int7 = 0
-
-        return int0,int1,int2,int3,int4,int5,int6,int7
+        return int0,int1,int2,int3,int4,int5,int6
 
     ##
     # getReward
@@ -128,11 +125,31 @@ class AIPlayer(Player):
     ##
     def getReward(self, stateTup):
         if stateTup[0] == 1:
-            return -1.
-        elif stateTup[1] == 1:
             return 1.
         else:
             return -.01
+
+    ##
+    # ###################################################################################################################
+    def chooseNextMove(self, currentState):
+        randChance = self.randMoveChance()
+        if random.random() < randChance:
+            return random.choice(listAllLegalMoves(currentState))
+        else:
+            legals = listAllLegalMoves(currentState)
+            i = 0
+            nextMove = legals[i]
+            while self.getSimpleState(self.getFutureState(currentState, nextMove)) not in self.stateUtils:
+                i += 1
+                if i >= len(legals):
+                    return random.choice(listAllLegalMoves(currentState))
+                nextMove = legals[i]
+            for m in legals:
+                mSimple = self.getSimpleState(self.getFutureState(currentState, m))
+                nextMoveSimple = self.getSimpleState(self.getFutureState(currentState, nextMove))
+                if mSimple in self.stateUtils and nextMoveSimple in self.stateUtils and self.stateUtils[mSimple] > self.stateUtils[nextMoveSimple]:
+                    nextMove = m
+            return nextMove
 
     ##
     # getFutureState
@@ -219,60 +236,6 @@ class AIPlayer(Player):
 
         # return the modified copy of the original state
         return newState
-
-    ##
-    # evaluateState
-    #
-    # Description: The evaluateState method looks at a state and
-    #   assigns a value to the state based on how well the game is
-    #   going for the current player
-    #
-    # Parameters:
-    #   currentState - The State to evaluate
-    #
-    # Return: The score of the state on a scale of 0.0 to 1.0
-    #   where 0.0 is a loss and 1.0 is a victory and 0.5 is neutral
-    ##
-    def evaluateState(self, state):
-        # local constants for adjusting weights of evaluation
-        QUEEN_EDGE_MAP_WEIGHT = 0.01
-        BUILD_WORKER_WEIGHT = 0.1
-        MOVE_TOWARDS_QUEEN_WEIGHT = 0.005
-        QUEEN_HEALTH_WEIGHT = 0.025
-        QUEEN_ON_HILL_WEIGHT = 0.1
-
-        # get some references for use later
-        playerInv = state.inventories[state.whoseTurn]
-        enemyInv = state.inventories[1 - state.whoseTurn]
-        enemyQueen = enemyInv.getQueen()
-
-        # start the score at a semi-neutral value
-        score = 0.4
-
-        # check each of the player's ants
-        for ant in playerInv.ants:
-            if ant.type == QUEEN:
-                # keep the queen off of the anthill
-                if ant.coords == self.playerAnthill:
-                    score -= QUEEN_ON_HILL_WEIGHT
-                # keep the queen at the edge of the map
-                score -= QUEEN_EDGE_MAP_WEIGHT * ant.coords[1]
-            elif ant.type == WORKER:
-                # encourage building more workers
-                score += BUILD_WORKER_WEIGHT
-                if enemyQueen:
-                    # move workers towards queen
-                    score -= MOVE_TOWARDS_QUEEN_WEIGHT * (abs(ant.coords[0] - enemyQueen.coords[0]) +
-                                                          abs(ant.coords[1] - enemyQueen.coords[1]))
-
-        if enemyQueen:
-            # encourage damaging the enemy queen
-            score += QUEEN_HEALTH_WEIGHT * (UNIT_STATS[QUEEN][HEALTH] - enemyQueen.health)
-        else:
-            score += QUEEN_HEALTH_WEIGHT * UNIT_STATS[QUEEN][HEALTH]
-
-        # return the evaluation score of the state
-        return score
            
     ##
     # getPlacement
@@ -288,7 +251,6 @@ class AIPlayer(Player):
     # Return: A list of coordinates of where the constructions are to be placed
     ##
     def getPlacement(self, currentState):
-        self.foodList = []
         #implemented by students to return their next move
         if currentState.phase == SETUP_PHASE_1:    #stuff on my side
             numToPlace = 11
@@ -336,25 +298,59 @@ class AIPlayer(Player):
     # Return: The Move to be made
     ##
     def getMove(self, currentState):        
-        # #fill the food locations list
-        # if not self.foodList:
-        #     for f in getConstrList(currentState, NEUTRAL, (FOOD,)):
-        #         self.foodList.append(f)
-        #
-        # self.setNeuralInputs(currentState)
+        nextMove = self.chooseNextMove(currentState)
 
-        # # create a root node to build our search tree from
-        # rootNode = treeNode.copy()
-        # rootNode["potential_state"] = currentState
-
-        # return the best move, found by recursively searching potential moves
-        #best = self.exploreTree(rootNode, 0)["move"]
-        best = max([(self.evaluateState(self.getFutureState(currentState, m)), m) for m in listAllLegalMoves(currentState)])[1]
-        if best.moveType == END:
-            print "END TURN"
+        simpleState = self.getSimpleState(currentState)
+        util = None
+        if simpleState in self.stateUtils:
+            utilTup = self.stateUtils[simpleState]
+            util = utilTup[0]
+            stateCount = utilTup[1]
         else:
-            print "SIMPLE: %s" % (self.getSimpleState(self.getFutureState(currentState, best)),)
-        return best
+            util = self.getReward(simpleState)
+            self.stateUtils[simpleState] = (util, 1)
+            stateCount = 1
+
+        nextSimpleState = self.getSimpleState(self.getFutureState(currentState, nextMove))
+        if nextSimpleState in self.stateUtils:
+            nextUtil = self.stateUtils[nextSimpleState][0]
+        else:
+            nextUtil = self.getReward(nextSimpleState)
+            self.stateUtils[nextSimpleState] = (nextUtil, 1)
+
+        self.stateUtils[simpleState] = (util + self.alpha(stateCount) * (self.getReward(simpleState) + GAMMA * nextUtil - util), stateCount + 1)
+
+        # if nextMove.moveType == END:
+        #     print "END TURN"
+        # else:
+        #     print "SIMPLE: %s" % (self.getSimpleState(self.getFutureState(currentState, nextMove)),)
+
+        self.moveCount += 1
+        self.moveCountThisGame += 1
+
+        return nextMove
+
+    ##
+    # alpha
+    #
+    # Description: Finds the learning rate for a state given how many times
+    #   we've visited it.
+    #
+    # Parameters:
+    #   visited - The number of times we've visited the state
+    #
+    # Return: The alpha value for that state
+    ##
+    def alpha(self, visited):
+        return 1./math.sqrt(1. + (visited - 1)/10.)#######################################################################################
+
+    ##
+    # randMoveChance
+    #
+    # Description: #######################################################################################################
+    ##
+    def randMoveChance(self):
+        return 1. - math.sqrt(self.moveCount)/1000. if self.moveCount < MAX_RAND_MOVES else 0.##############################
 
     ##
     # getAttack
@@ -379,4 +375,17 @@ class AIPlayer(Player):
     #   hasWon - True if the player won the game. False if they lost (Boolean)
     #
     def registerWin(self, hasWon):
-        pass
+        # for k in self.stateUtils:
+        #     print `k` + `self.stateUtils[k]`
+        #     print self.alpha(self.stateUtils[k][1])
+        print "Util table size: " + `len(self.stateUtils)`
+        print "Final move count this game: " + `self.moveCountThisGame`
+        print "Total agent move count: " + `self.moveCount`
+        print "Random move chance: " + `self.randMoveChance()`
+        self.moveCountThisGame = 0
+        try:
+            with open("AI/utils.p", 'w+b') as f:
+                pickle.dump((self.stateUtils, self.moveCount), f, 0)
+                print "Utils written to file."
+        except IOError:
+            print "ERROR: Failed writing utils to file."
